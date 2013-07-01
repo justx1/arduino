b3e1d6039bf88647cbff8b330f852a2b865667a5
justx1 <gwiesinger@gmail.com>
2013-06-30 17:40:03 -0700

    2

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,24 +1,55 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
+//used to store what we receive
+int incomingData = 0;
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+//the pin on which you connect the anode (+) of the LED
+const int temperaturePin = 0;
+const int ledPin =  13;
+float voltage, degreesC, degreesF;
+
+ 
+//this function runs only once (after reset)
+void setup(){
+  //open the serial port and set data rate to 9600 bps
+  Serial.begin(9600); 
+  //set the pin we use to OUTPUT so we can power-up the LED
+  pinMode(ledPin, OUTPUT); 
+  //send our status
+  Serial.println("Program started.");
+  Serial.print("The LED connected to GND and PIN ");
+  Serial.print(ledPin, DEC);
+  Serial.println(" will be on if you send '1' or off if you send anything else!");
+}
+ 
+//this function runs continuosly (after setup)
+void loop(){
+ 
+ voltage = getVoltage(temperaturePin);
+ degreesC = (voltage - 0.5) * 100.0;
+ 
+  //look if we have data
+  if (Serial.available() > 0) {
+      //get the sent data
+      incomingData = Serial.read();
+      Serial.print("We received : ");
+      Serial.println(incomingData);
+      Serial.print("The LED is : ");
+      //49 = ASCII code for '1'
+      if (incomingData == 49){ 
+        digitalWrite(ledPin, HIGH);
+        Serial.println("ON");
+      } else {
+        digitalWrite(ledPin, LOW);
+        Serial.println("OFF");
+      }
+      Serial.println();
+      Serial.print("  deg C: ");
+      Serial.print(degreesC);
+      Serial.print("  V: ");
+      Serial.print(voltage);
+
+  }
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+float getVoltage(int pin) {
+  return (analogRead(pin) * 0.004882814);
 }
